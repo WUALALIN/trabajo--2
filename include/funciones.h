c48ff588ac8f1261546875b59ff9fc462053e654
Alan Sánchez <alasanch@umag.cl>
2026-05-06 15:13:05 -0300

    avance relación a funciones de búsqueda

--- include/funciones.h
+++ include/funciones.h
@@ -23,9 +23,18 @@ void selectionSort(Deportista *a, int n, int criterio);
 void insertionSort(Deportista *a, int n, int criterio);
 void cocktailSort(Deportista *a, int n, int criterio);
 
-// Búsqueda
+// Búsqueda Clásica
 void busquedaSecuencial(Deportista *arreglo, int cantidad, int idBuscar);
 void busquedaBinaria(Deportista *arreglo, int cantidad, int idBuscar);
+
+// --- Nuevas Búsquedas ---
+int busquedaBinariaRecursiva(Deportista *arr, int inicio, int fin, int idBuscar);
+void busquedaBinariaRango(Deportista *arr, int n, int competenciasBuscar);
+int busquedaExponencial(Deportista *arr, int n, int idBuscar);
+int busquedaInterpolacion(Deportista *arr, int n, int idBuscar);
+
+// Función auxiliar para imprimir el resultado de las búsquedas que retornan índice
+void imprimirResultadoBusqueda(Deportista *arr, int indice, int valorBuscado, const char* algoritmo);
 
 // Experimentos de tiempo
 void copiarArreglo(Deportista *origen, Deportista *destino, int n);
